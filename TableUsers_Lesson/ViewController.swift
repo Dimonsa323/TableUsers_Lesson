52caf7d67855bd433c99e6457e1321a86b9cafda
Dima Hubenia <dima96gubenya@gmail.com>
2022-09-22 20:55:12 +0300

    Initial Commit

---
+++ TableUsers_Lesson/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TableUsers_Lesson
+//
+//  Created by Дима Губеня on 22.09.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
